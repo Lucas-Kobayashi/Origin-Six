c315a5f59b95f7723bd9e00e22c950479f79db29
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-04-01 11:03:40 -0300

    Sobre finalizado

--- script/main.js
+++ script/main.js
@@ -1,1 +1,18 @@
+// Toggle menu on and off
+
 const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+}
+
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
+}
